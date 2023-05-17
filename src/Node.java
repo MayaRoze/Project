d88de153b6c1dab217e80c7834717a923be506ad
MayaRoze <rmaya@campus.technion.ac.il>
2023-05-17 10:02:06 +0300

    Added methods in Node class, 17.5.23 morning

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,25 @@
 public class Node {
-    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    private State currentState;
+    private Node previousNode;
+    private Action actionToGetHere;
 
+    public Node(State currentState, Node previousNode, Action actionToGetHere) {
+        this.currentState = currentState;
+        this.previousNode = previousNode;
+        this.actionToGetHere = actionToGetHere;
+    }
 
+    public Node[] expand() {
+        Action[] actions = currentState.actions();
+        Node[] expansion = new Node[actions.length];
+        for (int i = 0; i < actions.length; i++) {
+            State newState = currentState.result(actions[i]);
+            expansion[i] = new Node(newState, this, actions[i]);
+        }
+        return expansion;
+    }
+
+    public int heuristicValue() {
+        return 0; //TODO: implement
+    }
 }
